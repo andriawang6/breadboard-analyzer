bc3f064277069aff4b3901650be41712b17015d0
jessicaliu06 <162812080+jessicaliu06@users.noreply.github.com>
2024-09-27 23:03:03 -0400

    Logic processing (#3)
    * add skeleton code for logic processing
    
    * add example endpoints array
    
    * build row_binds map
    
    * make create_relationships function and update row binds
    
    * changed order of expected output
    
    * removed function calls
    
    * finalized bind_rows()
    
    * separated tests from implementation
    
    * moved tests into src
    
    * added logic to determine inputs and outputs
    
    * deleted pycache
    
    ---------
    
    Co-authored-by: andriawang6 <andria@gatech.edu>

--- src/test_logic.py
+++ src/test_logic.py
@@ -11,44 +11,44 @@ chip_coords["chip2"] = ["74HCT00", (5, 12), (5, 13), (5, 14), (5, 15), (5, 16), 
                                    (6, 18), (6, 17), (6, 16), (6, 15), (6, 14), (6, 13), (6, 12)]
 
 chip_info = {}
-chip_info["74HCT04"] = ["1A", "1Y", "2A", "2Y", "3A", "3Y", "GND", "4Y", "4A", "5Y", "5A", "6Y", "6A", "VCC"]
-chip_info["74HCT00"] = ["1A", "1B", "1Y", "2A", "2B", "2Y", "GND", "3Y", "3A", "3B", "4Y", "4A", "4B", "VCC"]
+chip_info["74HCT04"] = ["NOT", "1A", "1Y", "2A", "2Y", "3A", "3Y", "GND", "4Y", "4A", "5Y", "5A", "6Y", "6A", "VCC"]
+chip_info["74HCT00"] = ["NAND", "1A", "1B", "1Y", "2A", "2B", "2Y", "GND", "3Y", "3A", "3B", "4Y", "4A", "4B", "VCC"]
 
 # what bind_rows() should output: 
 row_binds = {}
 # LEFT SIDE OF NOT
-row_binds["3L"] = "1Achip1"
-row_binds["4L"] = "1Ychip1"
-row_binds["5L"] = "2Achip1"
-row_binds["6L"] = "2Ychip1"
-row_binds["7L"] = "3Achip1"
-row_binds["8L"] = "3Ychip1"
-row_binds["9L"] = "GNDchip1"
+row_binds["3L"] = "1A_chip1"
+row_binds["4L"] = "1Y_chip1"
+row_binds["5L"] = "2A_chip1"
+row_binds["6L"] = "2Y_chip1"
+row_binds["7L"] = "3A_chip1"
+row_binds["8L"] = "3Y_chip1"
+row_binds["9L"] = "GND_chip1"
 # RIGHT SIDE OF NOT 
-row_binds["9R"] = "4Ychip1"
-row_binds["8R"] = "4Achip1"
-row_binds["7R"] = "5Ychip1"
-row_binds["6R"] = "5Achip1"
-row_binds["5R"] = "6Ychip1"
-row_binds["4R"] = "6Achip1"
-row_binds["3R"] = "VCCchip1"
+row_binds["9R"] = "4Y_chip1"
+row_binds["8R"] = "4A_chip1"
+row_binds["7R"] = "5Y_chip1"
+row_binds["6R"] = "5A_chip1"
+row_binds["5R"] = "6Y_chip1"
+row_binds["4R"] = "6A_chip1"
+row_binds["3R"] = "VCC_chip1"
 
 # LEFT SIDE OF NAND
-row_binds["12L"] = "1Achip2"
-row_binds["13L"] = "1Bchip2"
-row_binds["14L"] = "1Ychip2"
-row_binds["15L"] = "2Achip2"
-row_binds["16L"] = "2Bchip2"
-row_binds["17L"] = "2Ychip2"
-row_binds["18L"] = "GNDchip2"
+row_binds["12L"] = "1A_chip2"
+row_binds["13L"] = "1B_chip2"
+row_binds["14L"] = "1Y_chip2"
+row_binds["15L"] = "2A_chip2"
+row_binds["16L"] = "2B_chip2"
+row_binds["17L"] = "2Y_chip2"
+row_binds["18L"] = "GND_chip2"
 # RIGHT SIDE OF NAND
-row_binds["18R"] = "3Ychip2"
-row_binds["17R"] = "3Achip2"
-row_binds["16R"] = "3Bchip2"
-row_binds["15R"] = "4Ychip2"
-row_binds["14R"] = "4Achip2"
-row_binds["13R"] = "4Bchip2"
-row_binds["12R"] = "VCCchip2"
+row_binds["18R"] = "3Y_chip2"
+row_binds["17R"] = "3A_chip2"
+row_binds["16R"] = "3B_chip2"
+row_binds["15R"] = "4Y_chip2"
+row_binds["14R"] = "4A_chip2"
+row_binds["13R"] = "4B_chip2"
+row_binds["12R"] = "VCC_chip2"
 
 endpoints = [[(4, 0), (4, 3)], 
              [(4, 4), (4, 12)], 
@@ -64,6 +64,7 @@ for key in row_binds:
         print(f"{result[key]}, {row_binds[key]}, {key}")
 
 print("test create_relationships()")
-b, v = logic_processing.create_relationships(endpoints, result, MIDDLE, {})
-print(b)
-print(v)
+c, i, o = logic_processing.create_relationships(endpoints, result, MIDDLE)
+print(c)
+print(list(i))
+print(list(o))
