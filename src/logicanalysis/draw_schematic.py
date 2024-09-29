5055891f23d750b1b50d91577d1b8f1a1fdde684
andriawang6 <andria@gatech.edu>
2024-09-28 21:19:22 -0400

    change schematic return type to byte array

--- src/logicanalysis/draw_schematic.py
+++ src/logicanalysis/draw_schematic.py
@@ -19,7 +19,9 @@ def reformat_expr(logic_expr):
 def gen_schematic(raw_expr):
     formatted_expr, output_label = reformat_expr(raw_expr)
     drawing = logicparse(formatted_expr, gateW=2.5, gateH=2, outlabel=output_label)
-    drawing.draw()
-    drawing.save("schematic.svg")
+    # drawing.draw()
+    # drawing.save("schematic.svg")
+    return drawing.get_imagedata('svg')
 
-gen_schematic("((A)'*(C)')'=B")
+# res = gen_schematic("((A)'*(C)')'=B")
+# print(res)
